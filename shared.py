f30a2a32ff1b00f761a6b0c00656b1818c457e15
kpieczynski <kpieczynski@inpost.pl>
2023-02-22 20:25:06 +0100

    fix path to recordings

--- shared.py
+++ shared.py
@@ -1,3 +1,4 @@
-RECORDING_PATH = "C:\\Users\\kpieczynski\\Desktop\\ecordingPytho"
+import os
+RECORDING_PATH = os.path.dirname(os.path.abspath(__file__)) + '\\recordings'
 MOUSE_RECORDING_FILENAME = "mouse.bin"
 KEYBOARD_RECORDING_FILENAME = "keyboard.bin"
